7e8e3803b650eb26a91dde08a2d8da7d6ab9e55d
yuyc <yuyc@g08fnstd100537.g08.fujitsu.local>
2015-02-12 14:55:08 +0800

    Ｃ＋＋　单链表实现

--- CppLinkedList/LinkedList/LinkedList.h
+++ CppLinkedList/LinkedList/LinkedList.h
@@ -8,27 +8,64 @@
 #ifndef LINKEDLIST_H
 #define	LINKEDLIST_H
 
-class LinkedList {
-typedef struct element{
-    struct element* _next;
-    int _data;
-}listElement;
+#include"../ListElement/ListElement.h"
+#include <cstdlib>
+#include <iostream>
 
+using namespace std;
+
+template<class T>
+class LinkedList {
 public:
-    LinkedList();
-    virtual ~LinkedList();
+    LinkedList():_head(NULL){}
+    virtual ~LinkedList()
+    {
+        if (_head){
+            ListElement<T>* cur = NULL;
+            do{
+                cur = _head;
+                _head = cur->getNext();
+                delete cur;
+            }while(cur);
+        }        
+    }
     
-    bool insertInfront(int data);
+    bool insertInfront(const T& data)
+    {
+        ListElement<T> *newElem = new ListElement<T>(data);
+        if (!newElem) {
+            cout << "error happened @ insertInfront"<<endl;
+            return false;
+        }
+
+        newElem->setValue(data);
+        newElem->setNext(_head);      
+        this->setHead(newElem);
+
+        return true;            
+    }
+    //bool delNode(T data); 
     
-    bool delNode(int data); 
+    inline void setHead(ListElement<T>* head){ _head = head; }
+    inline ListElement<T>* getHead() const { return _head; }
     
-    void printList();
+    void printList()
+    {
+        ListElement<T> *head = this->getHead();
+        if( !head ) return;
+
+        int i=0;
+        ListElement<T> *cur = head;
+        while(cur){
+           cout<<"No:"<<i <<" is " << cur->getValue() <<endl;
+           cur = cur->getNext();
+           i++;
+        }        
+    }
     
 private:
-    listElement* _head;
-    listElement* _tail;
+    ListElement<T>* _head;
 
-    LinkedList(const LinkedList& orig){}
 };
 
 #endif	/* LINKEDLIST_H */
